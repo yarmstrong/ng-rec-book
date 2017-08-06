c150122894d082151c8878541cb7c8b29cb9e3fd
yarmstrong <ymir.armstrong@yahoo.com>
2017-08-06 19:21:44 +0800

    angular-recipe-book

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAuMElJYfLnW7S7Jo2JGXH31HCObrqll4E',
+      authDomain: 'ng-xxx-test.firebaseapp.com'
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RoutingModule } from 'app/app-routing.module';
+import { SharedModule } from 'app/shared/shared.module';
+import { ShoppingListModule } from 'app/shopping-list/shopping-list.module';
+import { AuthModule } from 'app/auth/auth.module';
+import { CoreModule } from 'app/core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RoutingModule,
+    SharedModule,
+    ShoppingListModule,
+    AuthModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
