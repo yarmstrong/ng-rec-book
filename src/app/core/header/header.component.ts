8a2ec79aadb7ad19d32c5973548aad230045c634
yarmstrong <ymir.armstrong@yahoo.com>
2017-09-14 23:09:41 +0800

    fixed the menu burger icon

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { DataStorageService } from 'app/shared/data-storage.service';
@@ -6,9 +6,13 @@ import { AuthService } from 'app/auth/auth.service';
 
 @Component({
     selector: 'app-header',
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  @ViewChild('menuicon') menuicon: ElementRef;
+  @ViewChild('menucontent') menucontent: ElementRef;
+  isOpen = false;
   /**
    * AoT error: Property 'authService' is private and only accessible within class 'HeaderComponent'.
    * so on [ngIf]="!authService.isAuthenticated()", we need to do create a fucntion that
@@ -16,7 +20,8 @@ export class HeaderComponent {
    */
   constructor(private dataStorageService: DataStorageService,
               private authService: AuthService,
-              private router: Router) {}
+              private router: Router,
+              private renderer: Renderer2) {}
   isAuthenticated() {
     return this.authService.isAuthenticated();
   }
@@ -32,5 +37,17 @@ export class HeaderComponent {
   onLogout() {
     this.authService.logout();
     this.router.navigate(['/signin']);
+  }
+  onToggle() {
+    /* this is for the toggling the hamburger menu */
+    if (!this.isOpen) {
+      this.isOpen = true;
+      this.renderer.addClass(this.menucontent.nativeElement, 'show');
+      this.renderer.addClass(this.menuicon.nativeElement, 'site-header__menu-icon--close-x');
+    } else {
+      this.isOpen = false;
+      this.renderer.removeClass(this.menucontent.nativeElement, 'show');
+      this.renderer.removeClass(this.menuicon.nativeElement, 'site-header__menu-icon--close-x');
+    }
   }
 }
